2186844aeb783a526e3ddd55d540ddb7f569c651
Connor Stack <cstack@umich.edu>
2014-03-16 18:57:19 -0400

    Add a random frequency envelope and gain envelope to each sound.

--- js/index.js
+++ js/index.js
@@ -5,15 +5,49 @@ var Sound = function() {
     return waveforms[Math.floor(Math.random()*waveforms.length)];
   }
 
-  this.freq = Math.random()*800 + 200;
-  this.waveform = randomWaveform();
-  this.duration = 1000; // in ms
+  var randomGainEnvelope = function() {
+    var attackTime = Math.random() * 300;
+    var sustainValue = Math.random() * 0.5 + 0.5;
+    var decayTime = Math.random() * 300;
+    var sustainTime = Math.random() * 500;
+    var releaseTime = Math.random() * 300;
+    var table = [0,
+      [1, attackTime],
+      [sustainValue, decayTime],
+      [sustainValue, sustainTime],
+      [0, releaseTime]];
+    return table;
+  }
 
-  this.sound = T(this.waveform, {freq:this.freq});
-  this.sound.set({mul:0.25});
+  var randomFreqEnvelope = function() {
+    var table = [Math.floor(Math.random() * 800 + 200)];
+    var numTransitions = Math.floor(Math.random() * 3);
+    for (var i = 0; i < numTransitions; i++) {
+      table.push([Math.random() * 800 + 200, Math.floor(Math.random()*500)]);
+    }
+    return table;
+  }
+
+  this.freq = T("env", {table:randomFreqEnvelope()});
+  this.waveform = randomWaveform();
+
+  var sound = T(this.waveform, {freq:this.freq});
+  sound = T("env", {table: randomGainEnvelope()}, sound);
+  sound.set({mul:0.25});
+
+  sound.on("ended", function(){
+    sound.pause();
+  });
+
+  this.sound = sound;
+
+  this.reset = function() {
+    this.freq.bang();
+    return this.sound.bang();
+  }
 
   this.play = function() {
-    this.sound.bang().play();
+    this.reset().play();
   }
 
   this.pause = function() {
@@ -25,16 +59,21 @@ var Sound = function() {
     var context = api.context;
     var gainNode = context.createGain();
     var rec = new Recorder(gainNode, {workerPath:"js/recorderWorker.js"});
-    api.append(this.sound);
-    api.send(gainNode);
+
+    var soundToPlay = this.reset();
+    soundToPlay.once("ended", function(){
+      setTimeout(function(){
+        rec.stop();
+          rec.exportWAV(function(blob) {
+          console.log(blob);
+          saveAs(blob, "test.wav");
+        });
+      }, 200); // Extra time at end of track
+    });
+
+    api.append(soundToPlay);
     rec.record();
-    setTimeout(function() {
-      rec.stop();
-      rec.exportWAV(function(blob) {
-        console.log(blob);
-        saveAs(blob, "test.wav");
-      });
-    }, this.duration);
+    api.send(gainNode);
   }
 }
 
@@ -57,7 +96,6 @@ $(function(){
       sound.play();
       activeBackground.stop().fadeIn(100);
     }, function(){
-      sound.pause();
       activeBackground.stop().fadeOut(100);
     });
     var downloadButton = $("<button>Download</button>");
