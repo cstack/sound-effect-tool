966b013a9e3bf854f84df14233a12a7f84eaf648
Connor Stack <cstack@umich.edu>
2014-03-16 22:32:56 -0400

    Mutate sounds on click

--- js/index.js
+++ js/index.js
@@ -1,49 +1,102 @@
-var Sound = function() {
+var Sound = function(seedSound) {
   var waveforms = ["sin", "saw", "square"];
 
   var randomWaveform = function() {
     return waveforms[Math.floor(Math.random()*waveforms.length)];
   }
 
-  var randomGainEnvelope = function() {
-    var attackTime = Math.random() * 300;
-    var sustainValue = Math.random() * 0.5 + 0.5;
-    var decayTime = Math.random() * 300;
-    var sustainTime = Math.random() * 500;
-    var releaseTime = Math.random() * 300;
+  var randomGainTable = function() {
+    this.attackTime = Math.random() * 300;
+    this.sustainValue = Math.random() * 0.5 + 0.5;
+    this.decayTime = Math.random() * 300;
+    this.sustainTime = Math.random() * 500;
+    this.releaseTime = Math.random() * 300;
     var table = [0,
-      [1, attackTime],
-      [sustainValue, decayTime],
-      [sustainValue, sustainTime],
-      [0, releaseTime]];
+      [1, this.attackTime],
+      [this.sustainValue, this.decayTime],
+      [this.sustainValue, this.sustainTime],
+      [0, this.releaseTime]];
     return table;
   }
 
-  var randomFreqEnvelope = function() {
+  var mutateGainTable = function(originalTable) {
+    var table = [0, [], [], [], []];
+    for (var i = 1; i <= 4; i++) {
+      for (var j = 0; j < 2; j++) {
+        table[i][j] = originalTable[i][j] * (0.9 + Math.random() * 0.2);
+      }
+    }
+    table[1][0] = 1;
+    table[4][0] = 0;
+    table[3][0] = table[2][0]; // sustain value
+
+    return table;
+  }
+
+  var randomFreqTransition = function() {
+    return [Math.random() * 800 + 200, Math.floor(Math.random()*500)];
+  }
+
+  var randomFreqTable = function() {
     var table = [Math.floor(Math.random() * 800 + 200)];
     var numTransitions = Math.floor(Math.random() * 3);
     for (var i = 0; i < numTransitions; i++) {
-      table.push([Math.random() * 800 + 200, Math.floor(Math.random()*500)]);
+      table.push(randomFreqTransition());
     }
     return table;
   }
 
-  this.freq = T("env", {table:randomFreqEnvelope()});
-  this.waveform = randomWaveform();
+  var mutateFreqTable = function(originalTable) {
+    var table = [];
+    table.push(originalTable[0] + Math.random() * 200 - 100);
+    var numTransitions = originalTable.length;
+    for (var i = 1; i < numTransitions; i++) {
+      var freq = originalTable[i][0] * (0.9 + Math.random() * 0.2);
+      var time = originalTable[i][1] * (0.9 + Math.random() * 0.2);
+      table.push([freq, time]);
+    }
 
-  var sound = T(this.waveform, {freq:this.freq});
-  sound = T("env", {table: randomGainEnvelope()}, sound);
+    if (Math.random() < 0.2) {
+      table.push(randomFreqTable());
+    }
+
+    if (Math.random() < 0.2 && table.length > 1) {
+      table.pop(1);
+    }
+
+    return table;
+  }
+
+  if (typeof seedSound === "undefined") {
+    // Create a random sound
+
+    this.freqTable = randomFreqTable();
+    this.waveform = randomWaveform();
+    this.gainTable = randomGainTable();
+  } else {
+    // Create a mutated version of the seed sound
+
+    this.freqTable = mutateFreqTable(seedSound.freqTable);
+    if (Math.random() < 0.2) {
+      this.waveform = randomWaveform();
+    } else {
+      this.waveform = seedSound.waveform;
+    }
+    this.gainTable = mutateGainTable(seedSound.gainTable);
+  }
+
+  var freqEnvelope = T("env",{table:this.freqTable});
+  var sound = T(this.waveform, {freq:freqEnvelope});
+  sound = T("env", {table: this.gainTable}, sound);
   sound.set({mul:0.25});
 
   sound.on("ended", function(){
     sound.pause();
   });
 
-  this.sound = sound;
-
   this.reset = function() {
-    this.freq.bang();
-    return this.sound.bang();
+    freqEnvelope.bang();
+    return sound.bang();
   }
 
   this.play = function() {
@@ -51,7 +104,7 @@ var Sound = function() {
   }
 
   this.pause = function() {
-    this.sound.pause();
+    sound.pause();
   }
 
   this.save = function() {
@@ -98,10 +151,14 @@ $(function(){
     }, function(){
       activeBackground.stop().fadeOut(100);
     });
-    var downloadButton = $("<button>Download</button>");
+    var downloadButton = $("<a href=\#\ class=\"glyphicon glyphicon-download download\"></a>");
     $(this).append(downloadButton);
     downloadButton.click(function(){
       sound.save();
+    });
+
+    $(this).click(function(){
+      sound = new Sound(sound);
     });
   });
 });
